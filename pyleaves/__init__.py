dfcaa089801d261ca64ca8581eaaa28fd859f86c
Jacob Rose <jacobrose@brown.edu>
2020-02-05 22:05:17 -0500

    Added package-wide global variables RESOURCES_DIR and DATABASE_PATH, defined dynamically within pyleaves/pyleaves/__init__.py. These allow any functions defined in the repo to directly reference the absolute path of our main resources directory in pyleaves/pyleaves/leavesdb/resources/, in addition to the contents at that location, regardless of where the full pyleaves package is installed on the user's system

--- pyleaves/__init__.py
+++ pyleaves/__init__.py
@@ -5,3 +5,12 @@ from . import data_pipeline
 from . import leavesdb
 from . import models
 from . import utils
+
+
+
+from os.path import dirname, join
+#Location of repo database resource files
+RESOURCES_DIR = join(dirname(__file__),'leavesdb','resources')
+#Full dir+filename of SQLite database db file
+DATABASE_PATH = join(RESOURCES_DIR,'leavesdb.db')
+
