1b5d1f7dba6ad1ed58aac24649fd880caefd59f7
Jacob Rose <jacobrose@brown.edu>
2020-02-11 21:38:20 -0500

    added .ipynb_checkpoints to .gitignore

--- pyleaves/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from . import analysis
-from . import config
-from . import datasets
-from . import data_pipeline
-from . import leavesdb
-from . import models
-from . import utils
--- pyleaves/leavesdb/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from .db_utils import (summarize_db,
-					init_local_db
-				   )
-from . import db_query
-from . import tf_utils
-from . import db_utils
