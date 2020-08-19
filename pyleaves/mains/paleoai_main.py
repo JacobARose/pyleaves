128261bd0ae9c1c0972ab6eafdb651fe48397217
JacobARose <jacobrose@brown.edu>
2020-08-19 13:49:24 -0400

    shape

--- pyleaves/mains/paleoai_main.py
+++ pyleaves/mains/paleoai_main.py
@@ -238,7 +238,7 @@ def resize_image(image, shape=(512,512,3), resize_buffer_size=128, training=Fals
     if training:
         smallest_side = tf.minimum(shape[0], shape[1])
         image = iau._aspect_preserving_resize(image, smallest_side = smallest_side + resize_buffer_size)
-        image = tf.image.random_crop(image, (shape[0],shape[0]), seed=seed)
+        image = tf.image.random_crop(image, shape, seed=seed)
     else:
         image = tf.image.resize_with_pad(image, target_height=shape[0], target_width=shape[1])
 
