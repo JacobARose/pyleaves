e5c81f9c55ca2d37d39da2646f5c42f69ea0b7f7
JacobARose <jacobrose@brown.edu>
2020-08-20 17:18:11 -0400

    bash script edit +fold_id

--- pyleaves/scripts/bash_parallel.sh
+++ pyleaves/scripts/bash_parallel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-CUDA_VISIBLE_DEVICES=0 python '/home/jacob/projects/pyleaves/pyleaves/mains/train_parallel.py'  fold_id=0 stage_0.misc.use_tfrecords=False &
+CUDA_VISIBLE_DEVICES=0 python '/home/jacob/projects/pyleaves/pyleaves/mains/train_parallel.py'  +fold_id=0 stage_0.misc.use_tfrecords=False &
 
-CUDA_VISIBLE_DEVICES=1 python '/home/jacob/projects/pyleaves/pyleaves/mains/train_parallel.py'  fold_id=1 stage_0.misc.use_tfrecords=False &
+CUDA_VISIBLE_DEVICES=1 python '/home/jacob/projects/pyleaves/pyleaves/mains/train_parallel.py'  +fold_id=1 stage_0.misc.use_tfrecords=False &
 
-CUDA_VISIBLE_DEVICES=2 python '/home/jacob/projects/pyleaves/pyleaves/mains/train_parallel.py'  fold_id=2 stage_0.misc.use_tfrecords=False
+CUDA_VISIBLE_DEVICES=2 python '/home/jacob/projects/pyleaves/pyleaves/mains/train_parallel.py'  +fold_id=2 stage_0.misc.use_tfrecords=False
