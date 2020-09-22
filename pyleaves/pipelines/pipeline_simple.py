31f3263db2a333550173100ae82cb767832618cf
JacobARose <jacobrose@brown.edu>
2020-09-22 06:34:31 -0400

    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'

--- pyleaves/pipelines/pipeline_simple.py
+++ pyleaves/pipelines/pipeline_simple.py
@@ -38,6 +38,7 @@ from more_itertools import unzip
 from pprint import pprint
 import shutil
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import neptune
 from pathlib import Path
 import yaml
