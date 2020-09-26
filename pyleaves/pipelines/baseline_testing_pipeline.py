0ac6d16d27b4379a641c8370bc2e8d9894266a00
JacobARose <jacobrose@brown.edu>
2020-09-26 08:42:43 -0400

    python ~/projects/pyleaves/pyleaves/pipelines/baseline_testing_pipeline.py 'lr=1e-5,1e-4,1e-3,1e-2' 'weights=null,"imagenet"' hydra.launcher.n_jobs=2 hydra.launcher.verbose=1 num_epochs=40

--- pyleaves/pipelines/baseline_testing_pipeline.py
+++ pyleaves/pipelines/baseline_testing_pipeline.py
@@ -5,8 +5,7 @@
 python ~/projects/pyleaves/pyleaves/pipelines/baseline_testing_pipeline.py 
 
 
-
-
+python ~/projects/pyleaves/pyleaves/pipelines/baseline_testing_pipeline.py 'lr=1e-5,1e-4,1e-3,1e-2' 'weights=null,"imagenet"' hydra.launcher.n_jobs=2 hydra.launcher.verbose=1 num_epochs=40
 
     Raises:
         e: [description]
