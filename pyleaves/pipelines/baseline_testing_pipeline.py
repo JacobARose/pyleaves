388426126f856c43ddb2c4fbdbb5d6ad5a07c2f3
JacobARose <jacobrose@brown.edu>
2020-09-26 08:24:19 -0400

    model_config

--- pyleaves/pipelines/baseline_testing_pipeline.py
+++ pyleaves/pipelines/baseline_testing_pipeline.py
@@ -315,7 +315,7 @@ def main(config):
     model_config.input_shape = (*params.target_size,3)
 
     neptune_params = {}
-    for k,v in {**params.to_dict(), **model_config.to_dict()}.items():
+    for k,v in {**OmegaConf.to_container(params, resolve=True), **OmegaConf.to_container(model_config, resolve=True)}.items():
         if type(v)==dict:
             neptune_params[k] = str(v)
         else:
