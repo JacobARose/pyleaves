729b6a7515b12d40dad111c60b3728408780f6fb
JacobARose <jacobrose@brown.edu>
2020-09-27 12:33:48 -0400

    pdb

--- pyleaves/pipelines/baseline_testing_pipeline.py
+++ pyleaves/pipelines/baseline_testing_pipeline.py
@@ -355,7 +355,7 @@ def main(config):
     neptune.init(project_qualified_name=neptune_project_name)
 
     from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
-    callbacks = [TensorBoard(log_dir=params.log_dir, profile_batch=2),
+    callbacks = [TensorBoard(log_dir=params.log_dir, profile_batch=0),
                 NeptuneMonitor(),
                 EarlyStopping(monitor=params.early_stopping.monitor,
                             patience=params.early_stopping.patience,
@@ -367,20 +367,21 @@ def main(config):
     with neptune.create_experiment(name=neptune_experiment_name, params=neptune_params) as experiment:
         model.summary(print_fn=lambda x: neptune.log_text('model_summary', x))
 
-        print('[BEGINNING TRAINING]')
-        try:
-            history = model.fit(train_data,
-                                epochs=params.num_epochs,
-                                callbacks=callbacks,
-                                validation_data=val_data,
-                                validation_freq=1,
-                                shuffle=True,
-                                steps_per_epoch=steps_per_epoch,
-                                validation_steps=validation_steps,
-                                verbose=1)
+        # print('[BEGINNING TRAINING]')
+        # try:
+        #     history = model.fit(train_data,
+        #                         epochs=params.num_epochs,
+        #                         callbacks=callbacks,
+        #                         validation_data=val_data,
+        #                         validation_freq=1,
+        #                         shuffle=True,
+        #                         steps_per_epoch=steps_per_epoch,
+        #                         validation_steps=validation_steps,
+        #                         verbose=1)
 
-        except Exception as e:
-            raise e
+        # except Exception as e:
+        #     raise e
+        print('skipping traing for pdb')
         from pyleaves.utils.pipeline_utils import evaluate_performance
 
         model.save(config.saved_model_path)
@@ -388,6 +389,7 @@ def main(config):
         print(f'Saved trained model to {config.saved_model_path}')
         subset='test'
         y, y_hat, y_prob = evaluate(model, test_data, experiment=experiment, subset=subset)
+        import pdb;pdb.set_trace()
         predictions = pd.DataFrame({'y':y,'y_pred':y_hat,'y_prob':y_prob})
         log_table(f'{subset}_labels_w_predictions',predictions, experiment=experiment)
         print('TEST RESULTS:')
