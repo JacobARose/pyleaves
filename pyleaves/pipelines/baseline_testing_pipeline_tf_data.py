296948033e962ecb8d849a7f92a9fee05a8adb82
JacobARose <jacobrose@brown.edu>
2020-09-27 20:37:07 -0400

    OmegaConf.to_container(params.tags, resolve=True)

--- pyleaves/pipelines/baseline_testing_pipeline_tf_data.py
+++ pyleaves/pipelines/baseline_testing_pipeline_tf_data.py
@@ -350,13 +350,13 @@ def main(config):
     upload_source_files=[os.path.join(os.path.dirname(__file__),'*.py')]
 
     # neptune_experiment_name = config.misc.experiment_name
-    import pdb;pdb.set_trace()
+    tags = OmegaConf.to_container(params.tags, resolve=True)
 
 
     with neptune.create_experiment(name=neptune_experiment_name,
                                    params=neptune_params,
                                    upload_source_files=upload_source_files,
-                                   tags=list([tag for tag in params.tags])) as experiment:
+                                   tags=tags) as experiment:
         model.summary(print_fn=lambda x: neptune.log_text('model_summary', x))
 
         print('[BEGINNING TRAINING]')
