798a6ccbcaf122b51e48d230cd2dcfde83b81c8b
JacobARose <jacobrose@brown.edu>
2020-10-18 04:02:44 -0400

    fixed er up

--- pyleaves/utils/WandB_artifact_utils.py
+++ pyleaves/utils/WandB_artifact_utils.py
@@ -36,24 +36,36 @@ def load_Leaves_Minus_PNAS_dataset():
 
 
 
-def load_train_test_artifact(artifact_name='jrose/uncategorized/Leaves-PNAS:v1'):
+def load_train_test_artifact(artifact_uri='jrose/uncategorized/Leaves-PNAS:v1'):
     run = wandb.init()
 
-    artifact = run.use_artifact(artifact_name, type='dataset')
+    artifact = run.use_artifact(artifact_uri, type='dataset')
     artifact_dir = artifact.download()
-    print(artifact_dir)
+    print('artifact_dir =',artifact_dir)
     train_df = pd.read_csv(os.path.join(artifact_dir,'train.csv'),index_col='id')
     test_df = pd.read_csv(os.path.join(artifact_dir,'test.csv'),index_col='id')
-    
     return train_df, test_df
+
+
 
 
 def load_dataset_from_artifact(dataset_name='Fossil', threshold=4, test_size=0.3, version='latest'):
     train_size = 1 - test_size
+    
+    if dataset_name=='Fossil':
+        artifact_name = f'{dataset_name}_{threshold}_{int(train_size*100)}-{int(100*test_size)}:{version}'
+    elif dataset_name=='PNAS':
+        artifact_name = f'{dataset_name}_50-50:{version}'
+    elif dataset_name=='Leaves':
+        artifact_name = f'{dataset_name}_family_{threshold}_{int(train_size*100)}-{int(100*test_size)}:{version}'
+    elif dataset_name=='Leaves-PNAS':
+        artifact_name = f'{dataset_name}_{int(train_size*100)}-{int(100*test_size)}:{version}'
+    elif dataset_name in ['Leaves_in_PNAS', 'PNAS_in_Leaves']:
+        artifact_name = f'{dataset_name}_{int(train_size*100)}-{int(100*test_size)}:{version}'
+            
+    artifact_uri = f'brown-serre-lab/paleoai-project/{artifact_name}'
+    return load_train_test_artifact(artifact_uri=artifact_uri)
 
-    artifact_name = f'brown-serre-lab/paleoai-project/{dataset_name}_{threshold}_{int(train_size*100)}-{int(100*test_size)}:{version}'
-        
-    return load_train_test_artifact(artifact_name=artifact_name)
 
 
 
