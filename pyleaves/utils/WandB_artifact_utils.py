10f334ff1df6dad0b3c089f5f840f5f37bd39cd2
JacobARose <jacobrose@brown.edu>
2020-10-20 23:17:01 -0400

    pdb

--- pyleaves/utils/WandB_artifact_utils.py
+++ pyleaves/utils/WandB_artifact_utils.py
@@ -39,7 +39,8 @@ def load_Leaves_Minus_PNAS_dataset():
 def load_train_test_artifact(artifact_uri='jrose/uncategorized/Leaves-PNAS:v1', run=None):
     if run is None:
         run = wandb.init(reinit=True)
-        
+    
+
     with run:
         artifact = run.use_artifact(artifact_uri, type='dataset')
         artifact_dir = artifact.download()
@@ -66,6 +67,8 @@ def load_dataset_from_artifact(dataset_name='Fossil', threshold=4, test_size=0.3
     elif dataset_name in ['Leaves_in_PNAS', 'PNAS_in_Leaves']:
         artifact_name = f'{dataset_name}_{int(train_size*100)}-{int(100*test_size)}:{version}'
             
+
+    import pdb; pdb.set_trace()
     artifact_uri = f'brown-serre-lab/paleoai-project/{artifact_name}'
     return load_train_test_artifact(artifact_uri=artifact_uri, run=run)
 
