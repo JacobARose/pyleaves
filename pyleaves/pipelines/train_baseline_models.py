f98f06f585a9f5b206502db99a27bd8e9b87f6f9
JacobARose <jacobrose@brown.edu>
2020-11-20 06:17:56 -0500

    run = init_wandb_run(config_3, group=config_3.group)

--- pyleaves/pipelines/train_baseline_models.py
+++ pyleaves/pipelines/train_baseline_models.py
@@ -848,7 +848,7 @@ def finetune_trial(cli_args=None):
     config_2 = get_config(warmup_learning_rate=config_1.warmup_learning_rate/2, model_weights=model_weights, 
                           frozen_layers=(0,frozen_layer_sequence[1]), head_layer_units=config_1.head_layer_units,
                           num_epochs=initial_epoch+num_epochs_sequence[1], model_index=1, cli_args=cli_args)
-    # run = init_wandb_run(config_2, group=config_2.group)
+    run = init_wandb_run(config_2, group=config_2.group)
     model = fit_one_cycle(config_2, model=model, run=run, initial_epoch=initial_epoch, rebuild_head=False)
 
     print(f'Beginning stage 3 of finetune trial')
@@ -856,7 +856,7 @@ def finetune_trial(cli_args=None):
     config_3 = get_config(warmup_learning_rate=config_2.warmup_learning_rate/2, model_weights=model_weights,
                           frozen_layers=(0,frozen_layer_sequence[2]), head_layer_units=config_2.head_layer_units, 
                           num_epochs=initial_epoch+num_epochs_sequence[2], model_index=2, cli_args=cli_args)
-    # run = init_wandb_run(config_3, group=config_3.group)
+    run = init_wandb_run(config_3, group=config_3.group)
     model = fit_one_cycle(config_3, model=model, run=run, initial_epoch=initial_epoch, rebuild_head=False)
 
     model.save(config_3.model_path+'_final')
